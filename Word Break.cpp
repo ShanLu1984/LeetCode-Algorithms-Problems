fe3975b27e94dc39c1c04f80c236432757cee3ac
ShanLu1984 <31110848+ShanLu1984@users.noreply.github.com>
2017-10-19 23:29:00 -0700

    Update Word Break.cpp

--- Word Break.cpp
+++ Word Break.cpp
@@ -8,6 +8,7 @@ class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
         int n = s.size();
+        // put all the words in the Dict into a unorder_set, so the time for search is O(1)
         unordered_set<string> st;
         for (auto w : wordDict) {
             st.insert(w);
@@ -16,10 +17,10 @@ public:
         dp[0] = true;
         for (int i=1; i<=n; ++i) {
             for (int j=0; j<i; ++j) {
-                if (dp[j] == false) continue; 
+                if (dp[j] == false) continue; // dp[j] is false, then no need to check s[j,i-1];
                 string temp = s.substr(j, i-j);
-                if (st.find(temp) == st.end()) continue;
-                dp[i] = true;
+                if (st.find(temp) == st.end()) continue; //s[j,i-1] is not in Dict, so false;
+                dp[i] = true; // dp[j] is true and s[j,i-1] is in Dict, can be segmented, dp[i] = true;
                 break;
             }
         }
